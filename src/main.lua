2b2a088a9dc64eea52dff4c89d5b8462dd179906
Joan <joan.rivas.cepeda@gmail.com>
2023-10-04 23:15:57 -0300

    V1.2 Enemy and Player Stats completed

--- src/main.lua
+++ src/main.lua
@@ -6,14 +6,52 @@ height = love.graphics.getHeight()
 love.window.setMode(width, height, {fullscreen=false, fullscreentype = "desktop", borderless=false,centered=true, borderless=false})
 
 Player = {
+    posX = (width/2),
+    posY = (height/2),
+    speed = 40,
+    attack = 1,
+    life = 10,
+    level = 1,
+    exp = 0,
+    expMax = 50,
+    Height = 20,
+    Width = 20,
+    Score = 0,
+    Time = 0,
+    IFrame = 0
+}
+
+Enemy = {
     posX = 10,
     posY = 10,
-    speed = 10,
-    life = 3
+    damage = 1,
+    speed = 35,
+    life = 5,
+    exp = 5,
+    Height = 20,
+    Width = 20
 }
+
 end
 
+function Damage(Player, Enemy)
+    if (Player.IFrame == 0) then
+        if (Player.posY == Enemy.posY ) then
+
+            if (Player.posX == Enemy.posX) then
+                Player.IFrame = 10
+                Player.life = Player.life - 1
+            end
+        end
+    end
+end
+
+
 function love.update(dt)
+
+if  (Player.IFrame > 0) then
+    Player.IFrame = -1
+end
 
 if love.keyboard.isDown("d") then
     if   (Player.posX + Player.speed < width) then
@@ -23,7 +61,7 @@ end
 
 if love.keyboard.isDown("a") then
     if   (Player.posX - Player.speed > 0 ) then
-        Player.posX = Player.posX+(Player.speed*dt) 
+        Player.posX = Player.posX-(Player.speed*dt) 
     end
 end
 
@@ -48,5 +86,5 @@ function love.draw( )
     
 
     love.graphics.setColor(0,0.4,0.4)
-    love.graphics.rectangle("fill",Player.posX,Player.posY,20,20)
+    love.graphics.rectangle("fill",Player.posX,Player.posY,Player.Height,Player.Width)
 end
